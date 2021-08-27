05462e9575242905d8f2c1e66254c6be89c02935
RagulKanna <ragulragi619@gmail.com>
2021-08-27 12:26:12 +0530

    Added:To find the quotient and remainder of the number

--- src/com/Assignment/Basiccoreprogram.java
+++ src/com/Assignment/Basiccoreprogram.java
@@ -5,17 +5,13 @@ import java.util.Scanner;
 public class Basiccoreprogram {
         public static void main(String[] args) {
         Scanner y =new Scanner(System.in);
-        System.out.println("Enter the number to be factorize: ");
+        System.out.println("Enter the number to find quotient and remainder ");
         int num =y.nextInt();
-        System.out.println("Factor is ");
-        for (int i=2;i<num;i++)
-        {
-                while(num%i==0) {
-                        System.out.println(i);
-                        num = num / i;
-                }
-                }
-        if (num>2)
-                System.out.println(num);
-    }
+        System.out.println("Enter the divisor: ");
+        int div =y.nextInt();
+        int quotient=num / div;
+        int remainder=num % div;
+        System.out.println("The Quotient of the number is "+quotient);
+        System.out.println("The Remainder of the number is "+remainder);
+        }
 }
