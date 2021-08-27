1193cbc3f8320c88adeebf721c4727d732035f24
RagulKanna <ragulragi619@gmail.com>
2021-08-27 10:07:53 +0530

    Added:Checking Leap Year or not

--- src/com/Assignment/Basiccoreprogram.java
+++ src/com/Assignment/Basiccoreprogram.java
@@ -3,36 +3,21 @@ package com.Assignment;
 import java.util.Scanner;
 
 public class Basiccoreprogram {
-    public static int heads=0,tails=0;
     public static void main(String[] args) {
-        Scanner c=new Scanner(System.in);
-
-        System.out.println("Enter the positive value to flip a coin: ");
-        int flip=c.nextInt();
-        if (flip<0)
-            System.out.println("You Entered Wrong value");
+        Scanner y =new Scanner(System.in);
+        System.out.println("Enter the year consist of four digit: ");
+        int year =y.nextInt();
+        if(year < 1000 || year > 9999)
+        System.out.println("\nYou entered wrong year");
         else
+        {
+            if(year % 4 == 0)
+                System.out.println("\nEntered year is Leap Year");
+            else
+                System.out.println("\nEntered year is not a Leap year");
+        }
 
-            for(int i=0;i<flip;i++)
-            {
-
-                float coin= (float) Math.random();
-                System.out.printf("\nThe Value of probability of flip of coin: "+"%.2f",coin);
-                if( coin > 0.5 ) {
-                    heads = heads + 1;
-
-                }
-                else
-                    tails = tails + 1;
-
-            }
-            if(flip > 0) {
-                System.out.println("\nThe no of Heads We get: " + heads);
-                System.out.println("\nThe no of tails We get: " + tails);
-                int Perhead = (heads * 100) / flip;
-                int Pertail = (tails * 100) / flip;
-                System.out.println("\nThe Head Percentage is :" + Perhead + "%");
-                System.out.println("\nThe tail Percentage is :" + Pertail + "%");
-            }
     }
+
+
 }
