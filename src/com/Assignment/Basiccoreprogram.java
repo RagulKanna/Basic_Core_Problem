e323ed82e866f9bc454f089dd46ed1c50f5bcbe2
RagulKanna <ragulragi619@gmail.com>
2021-08-27 10:43:24 +0530

    Added:get Harmonic value

--- src/com/Assignment/Basiccoreprogram.java
+++ src/com/Assignment/Basiccoreprogram.java
@@ -3,18 +3,16 @@ package com.Assignment;
 import java.util.Scanner;
 
 public class Basiccoreprogram {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         Scanner y =new Scanner(System.in);
-        System.out.println("Enter the power of 2 upto you needed: ");
-        int pow =y.nextInt();
-        int value;
-        for (int i=1;i<=pow;i++)
+        System.out.println("Enter the harmonic number  upto you needed: ");
+        int num =y.nextInt();
+        int constant=1;
+        float value = 0.0f;
+        for (int i=1;i<=num;i++)
         {
-            value=(int)Math.pow(2,i);
-            System.out.println("\n The value of 2 power "+i+" is "+value);
+            value = value + (float) constant / i ;
         }
-
+        System.out.println("For The Harmonic value of "+num+" is "+value);
     }
-
-
 }
