f157ce0f2066652084c3967341d491f72e769592
RagulKanna <ragulragi619@gmail.com>
2021-08-27 13:02:39 +0530

    Added:To find the Largest number among three numbers

--- src/com/Assignment/Basiccoreprogram.java
+++ src/com/Assignment/Basiccoreprogram.java
@@ -5,11 +5,18 @@ import java.util.Scanner;
 public class Basiccoreprogram {
         public static void main(String[] args) {
         Scanner y =new Scanner(System.in);
-        System.out.println("Enter the alphabet is vowels or consonant:");
-        char alp =y.next().charAt(0);
-        if(alp == 'a' || alp == 'e' || alp == 'i' || alp == 'o' || alp == 'u')
-                 System.out.println("The character "+ alp +"  is vowel");
+        System.out.println("Enter three numbers to find greatest");
+                System.out.println("Enter first number");
+        int n1= y.nextInt();
+                System.out.println("Enter Second number");
+        int n2= y.nextInt();
+                System.out.println("Enter third number");
+        int n3= y.nextInt();
+        if(n1>n2 && n1>n3)
+                 System.out.println("The Number "+ n1 +"  is Largest among Three number");
+        else if(n2>n3)
+                System.out.println("The Number "+ n2 +"  is Largest among Three number");
         else
-                System.out.println("The character "+ alp +"  is Consonant");
+                System.out.println("The Number "+ n3 +"  is Largest among Three number");
         }
 }
