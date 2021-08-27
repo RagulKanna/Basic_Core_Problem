5df8dca793ba5356da3af5d1083ef6c125fe2592
RagulKanna <ragulragi619@gmail.com>
2021-08-27 12:54:36 +0530

    Added:To find the character is vowel or consonant

--- src/com/Assignment/Basiccoreprogram.java
+++ src/com/Assignment/Basiccoreprogram.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class Basiccoreprogram {
         public static void main(String[] args) {
         Scanner y =new Scanner(System.in);
-        System.out.println("Enter the number to find it is even or odd");
-        int num1 =y.nextInt();
-        if(num1 % 2 == 0)
-                 System.out.println("The number "+num1+"  is Even");
+        System.out.println("Enter the alphabet is vowels or consonant:");
+        char alp =y.next().charAt(0);
+        if(alp == 'a' || alp == 'e' || alp == 'i' || alp == 'o' || alp == 'u')
+                 System.out.println("The character "+ alp +"  is vowel");
         else
-                System.out.println("The number "+num1+"  is Odd");
+                System.out.println("The character "+ alp +"  is Consonant");
         }
 }
